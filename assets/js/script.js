2677712bc1d81b61461e059243d7335cee834c90
Lucca Giovanni <luccagiovannib@gmail.com>
2021-04-02 19:43:04 -0300

    Correções de estilo de código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,20 @@
+window.onload = function () {
+  var product = document.querySelector("#product");
+  var button = document.querySelector("#mainButton");
+  var list = document.querySelector(".carList");
 
-window.onload = function(){
-    var product = document.querySelector('#product');
-    var button = document.querySelector('#mainButton');
-    var list = document.querySelector('.carList');
+  button.addEventListener("click", addProduct);
 
+  function addProduct() {
+    var item = "<li>" + product.value + "</li>";
+    list.innerHTML += item;
+    product.value = "";
+    product.focus();
+  }
 
-    button.addEventListener("click", addProduct)
-
-    function addProduct(){
-        var item = '<li>' + product.value + '</li>';
-        list.innerHTML += item;
-        product.value = '';
-        product.focus();
+  product.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      addProduct();
     }
-
-    product.addEventListener("keypress", function(e) {
-        if (e.key === 'Enter') {
-            addProduct();
-        } 
-    })
-    
-}
+  });
+};
